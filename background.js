fb8e6a1956b3da7ff8b7d43e5e5b3c5d9544e9e4
aniki <aniki@dr.h>
2022-05-04 13:02:44 +0800

    version 1

--- background.js
+++ background.js
@@ -1,17 +1,55 @@
 let urls = []
 
-chrome.runtime.onInstalled.addListener(async () => { })
+chrome.runtime.onInstalled.addListener(async () => {
+    console.log("ext installed")
+    chrome.storage.local.get("urls", ({ urls: argUrls }) => {
+        // console.log("urls:", argUrls)
+        if (argUrls) {
+            urls = argUrls
+        }
+    })
+})
+
+function resetUrls() {
+    urls = []
+    chrome.storage.local.set({ urls }, () => {
+        // console.log("starage set ", urls)
+    })
+}
+
+function pushUrls(urlObj) {
+    for (const obj in urls) {
+        if (obj.url == urlObj.url) {
+            return;
+        }
+    }
+
+    urls.push(urlObj)
+    chrome.storage.local.set({ urls }, () => {
+        // console.log("starage set ", urls)
+    })
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("on message ", request)
     if (request.type == "getUrls") {
         sendResponse(urls)
     }
 
     if (request.type == "reset") {
-        urls = []
+        resetUrls([])
     }
 })
 
 chrome.webRequest.onBeforeSendHeaders.addListener(({ requestHeaders: headers, url }) => {
-    urls.push({ headers, url })
-}, { urls: ["https://*/*.m3u8"] }, ["requestHeaders", "extraHeaders"])
+    // console.log(headers, url)
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        pushUrls({
+            headers,
+            url,
+            title: tabs[0] ? tabs[0].title : "untitled",
+            pageUrl: tabs[0] ? tabs[0].url : "#"
+        })
+    })
+
+}, { urls: ["https://*/*.m3u8*"] }, ["requestHeaders", "extraHeaders"])
--- popup.js
+++ popup.js
@@ -1,40 +1,42 @@
 const text1 = document.getElementById("txt1")
 const ul = document.getElementById("ul")
 
-let hostUrl = "http://127.0.0.1:2000/m3u8"
+let hostUrl = "http://127.0.0.1:2022/m3u8"
 
 window.onload = function () {
     update()
 }
 
 function update() {
-    chrome.runtime.sendMessage({ type: "getUrls" }, urls => {
+    // console.log("before send message")
+    chrome.runtime.sendMessage(null, { type: "getUrls" }, {}, urls => {
+        // console.log("got message", urls)
         for (const it of urls) {
-            const { url, headers } = it
-            addLink(url, headers)
+            const { url, headers, title, pageUrl } = it
+            addLink(url, headers, title, pageUrl)
         }
     })
 }
 
-function addLink(url, headersArr = {}) {
+function addLink(url, headersArr, title, pageUrl) {
     const idx = url.lastIndexOf("/") + 1
     const link = document.createElement("a")
     link.setAttribute("href", url)
     link.setAttribute("title", url)
-    link.innerText = url.substr(idx)
+    link.innerText = title + "_" + url.substr(idx)
 
     const button = document.createElement("button")
+    button.classList = "btn btn-outline-primary btn-sm float-right"
     button.innerText = "download"
-
-    const headers = {}
-    headersArr.map(it => headers[it.name] = it.value)
-
     button.onclick = async function () {
         try {
+            const controller = new AbortController()
+            const timeoutId = setTimeout(() => controller.abort(), 1500)
             const res = await fetch(hostUrl, {
+                signal: controller.signal,
                 method: 'POST',
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ headers, url })
+                body: JSON.stringify({ headers, url, title })
             })
             json = await res.json()
             if (json.code == 0) {
@@ -44,13 +46,33 @@ function addLink(url, headersArr = {}) {
             }
         }
         catch (err) {
+            if (err instanceof DOMException) {
+                alert('timeout maybe downloading')
+                return
+            }
             alert('connect error check if server is not running')
         }
 
     }
 
+    const buttonSrc = document.createElement("button")
+    buttonSrc.classList = "btn btn-outline-primary btn-sm float-right ml-2"
+    buttonSrc.innerText = "src"
+    buttonSrc.onclick = function () {
+        if (pageUrl != "#") {
+            window.open(pageUrl)
+        }
+    }
+
+    const headers = {}
+    headersArr.map(it => headers[it.name] = it.value)
+
+
+
     const li = document.createElement("li")
+    li.classList = "list-group-item pb-0 pt-2 px-0"
     li.appendChild(link)
+    li.appendChild(buttonSrc)
     li.appendChild(button)
 
     ul.appendChild(li)
